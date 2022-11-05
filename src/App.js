f13098eb33e42a13e53fecc0d905fb62ac7bfd7a
Datodia <dato.diasamidze.02@gmail.com>
2022-11-05 11:12:14 +0400

    init: initialize project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import logo from './assets/logo.svg'
+import avatar from './assets/avatar.png'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+    </div>
+  );
+}
+
+export default App;
